de45485d78d53914172b7746144647bae948727e
bastion6429 <emarti43@ucsc.edu>
2019-10-29 15:30:42 -0700

    moving padding to right to see if it's fixed

--- discordBot.js
+++ discordBot.js
@@ -221,7 +221,7 @@ async function showChannelActivity(channel) {
   resultString = channels.reduce( (result, element) => {
     let formattedDate = element.last_cycle.split('/');
     formattedDate = new Date(formattedDate[1], formattedDate[0]);
-    let row = channel.guild.channels.get(element.channel_id).toString().padEnd(35, " ") +  element.message_count.toString().padStart(5, ' ') + formattedDate.toDateString().padStart(20, " ") + '\n';
+    let row = channel.guild.channels.get(element.channel_id).toString().padStart(30, " ") +  element.message_count.toString().padStart(5, ' ') + formattedDate.toDateString().padStart(20, " ") + '\n';
     return result + row;
     }, resultString);
   channel.send(resultString);
