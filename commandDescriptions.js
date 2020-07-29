fe371b4e391ce9ca6fccfdd9d96b304e32013abe
bastion6429 <emarti43@ucsc.edu>
2020-07-29 12:41:46 -0700

    fixed mistake :(

--- commandDescriptions.js
+++ commandDescriptions.js
@@ -2,7 +2,7 @@ const COMMAND_DESCRIPTIONS = {
   "!purge_images #channel_name": "removes all images/links in #channel_name",
   "!set_sweeper #channel_name": "removes images from user 6 minutes after posting from #chanel_name",
   "!add_channel #channel_name": "used to add channels to purge (admin only)",
-  "!show_": "displays current channels being monitored for purging",
-  "!show_monitored_channels": "displays server statistics (work in progress)"
+  "!show_monitored_channels": "displays current channels being monitored for purging",
+  "!server_stats": "displays server statistics (work in progress)"
 };
 module.exports = COMMAND_DESCRIPTIONS;
