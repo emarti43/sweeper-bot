82d15e00efef248350268fe43f69cbb8197529a4
emarti43 <emarti43@ucsc.edu>
2020-10-14 19:22:00 -0700

    fix channel usage

--- commandDescriptions.js
+++ commandDescriptions.js
@@ -1,5 +1,5 @@
 const COMMAND_DESCRIPTIONS = {
-  "!purge_images #channel_name": "removes all images/links in #channel_name",
+  "!purge_images [@username] #channel_name": "removes all images/links in #channel_name",
   "!set_sweeper #channel_name": "removes images from user 6 minutes after posting from #chanel_name",
   "!add_channel #channel_name": "used to add channels to purge (admin only)",
   "!remove_channel #channel_name": "used to remove channels to purge (admin only)",
